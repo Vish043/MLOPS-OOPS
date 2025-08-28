81254da50b38df983119331f834cd3404ee37974
Vish043 <vishalgaonkar2004@gmail.com>
2025-08-28 21:56:07 +0530

    ChatBook project

--- oops_proj.py
+++ oops_proj.py
@@ -11,16 +11,58 @@ class chatbook:
                                2. Press 2 to SignIn.
                                3. Press 3 to write a post.
                                4. Press 4 to message a friend.
-                               5. Press any other key to exit''')
+                               5. Press any other key to exit \n''')
         if user_inpt == "1":
-            pass
+            self.signup()
         elif user_inpt == "2":
-            pass
+            self.signin()
         elif user_inpt == "3":
-            pass
+            self.my_post()
         elif user_inpt == "4":
-            pass
+            self.sending()
         else:
             exit()
 
-obj = chatbook()
+    def signup(self):
+        email = input("enter your email here -> ")
+        pwd = input ("setup your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("You have signedup successfully !!")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("Please signup first by pressing  1 in the min menu ")
+        else :
+            uname = input("Enter yout email/username here -> ")
+            pwd = input("Enter your password here -> ")
+            if self.username == uname and self.password == pwd:
+                print("You have SignedIn successfully !!")
+                self.loggedIn = True
+            else:
+                print("Please input correct credentials")
+            print("\n")
+            self.menu()
+
+    def my_post(self):
+        if self.loggedIn == False:
+            print("Please SignIn first before writing a post")
+        else:
+            post = input("Enter your message here:\n")
+            print(f"{self.username} posted * {post} * ")
+        print("\n")
+        self.menu()
+
+    def sending(self):
+        if self.loggedIn == True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Who to send this msg? -> ")
+            print (f"Your message has been sent to {frnd}")
+        else:
+             print("Please SignIn first before sending a msg")
+        print("\n")
+        self.menu()
+
+#obj = chatbook()
--- rough.py
+++ rough.py
@@ -1,13 +1,17 @@
-lst = [1,2,3]
-my_str = "mlops playlist"
-my_int = 155
+#lst = [1,2,3]
+#my_str = "mlops playlist"
+#my_int = 155
 
 #print(type(my_str))
 
 #my_str=my_str.capitalize()
 #print(lst)
 
-a = 'x'
-b = 'y'
+#a = 'x'
+#b = 'y'
 
-print(a+b)
+#print(a+b)
+
+from oops_proj import chatbook 
+
+user1 = chatbook()
