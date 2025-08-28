57ca72a72958aa247a418ab22fba4d4813ec70a5
Vish043 <vishalgaonkar2004@gmail.com>
2025-08-28 20:44:48 +0530

    structure ready

---
+++ oops_proj.py
@@ -0,0 +1,26 @@
+class chatbook:
+    def __init__ (self):
+        self.username = ''
+        self.password = ''
+        self.loggedIn = False
+        self.menu()
+
+    def menu(self):
+        user_inpt = input('''Welcome to Chatbook how would you like to proceed?
+                               1. Press 1 to SignUp.
+                               2. Press 2 to SignIn.
+                               3. Press 3 to write a post.
+                               4. Press 4 to message a friend.
+                               5. Press any other key to exit''')
+        if user_inpt == "1":
+            pass
+        elif user_inpt == "2":
+            pass
+        elif user_inpt == "3":
+            pass
+        elif user_inpt == "4":
+            pass
+        else:
+            exit()
+
+obj = chatbook()
